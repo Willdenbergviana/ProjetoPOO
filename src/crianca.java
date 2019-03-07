2040c6e72589a4f9c29eef500260296aadbda9d3
04032052 <wviana.23@gmail.com>
2019-03-07 20:33:49 -0300

    Initial commit

---
+++ src/crianca.java
@@ -0,0 +1,12 @@
+class crianca {
+    String corCabelo;
+    String corPele;
+    double altura;
+    int idade;
+    void brincar(){
+        System.out.println("Brincando... ");
+        }
+        void correr(){
+            System.out.println(" Correndo... ");
+        }
+}
