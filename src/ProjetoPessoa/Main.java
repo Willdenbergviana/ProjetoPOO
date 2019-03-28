f67c33a470c083fe7c8efd84ebbdebd86ab652c7
04032052 <wviana.23@gmail.com>
2019-03-28 20:51:13 -0300

    exercicio 280319

---
+++ src/ProjetoPessoa/Main.java
@@ -0,0 +1,8 @@
+package ProjetoPessoa;
+
+public class Main {
+    public static void main(String args[]){
+        Pessoa p1 = new Pessoa("Pedro", "242424");
+
+    }
+}
---
+++ src/ProjetoPessoa/Pessoa.java
@@ -0,0 +1,29 @@
+package ProjetoPessoa;
+
+public class Pessoa {
+    String nome;
+    String cpf;
+
+    Pessoa( String nome, String cpf){
+         this.nome = nome;
+         this.cpf = cpf;
+
+    }
+
+    public void setnome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome = nome;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getCpf(){
+        return this.cpf = cpf;
+    }
+
+}
